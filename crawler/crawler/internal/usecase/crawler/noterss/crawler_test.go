9270e1c2d634306e537d16a61272851bb1492d5b
Taito Suzuki <suzuito3@gmail.com>
2023-11-07 18:17:07 +0900

    Add empty test files

---
+++ crawler/crawler/internal/usecase/crawler/noterss/crawler_test.go
@@ -0,0 +1,18 @@
+package noterss
+
+import "testing"
+
+func Test(t *testing.T) {
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+
+		})
+	}
+}
