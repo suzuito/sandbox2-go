4c9391759059f06e0cb55128c56203c7889b5d29
Taito Suzuki <suzuito3@gmail.com>
2023-11-08 22:17:29 +0900

    Add crawler/pkg/entity/timeseriesdata/note/time_series_data_note_article_test.go

--- crawler/pkg/entity/timeseriesdata/note/time_series_data_note_article_test.go
+++ crawler/pkg/entity/timeseriesdata/note/time_series_data_note_article_test.go
@@ -1,1 +1,18 @@
 package note
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/suzuito/sandbox2-go/crawler/pkg/entity/timeseriesdata"
+)
+
+func TestTimeSeriesDataNoteArticle(t *testing.T) {
+	a := TimeSeriesDataNoteArticle{
+		URL:         "https://www.example.com",
+		PublishedAt: time.Date(1, 2, 3, 4, 5, 6, 7, time.UTC),
+	}
+	assert.Equal(t, timeseriesdata.TimeSeriesDataID("https---www.example.com"), a.GetID())
+	assert.Equal(t, time.Date(1, 2, 3, 4, 5, 6, 7, time.UTC), a.GetPublishedAt())
+}
