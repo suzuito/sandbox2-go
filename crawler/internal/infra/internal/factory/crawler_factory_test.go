06a21f429d6ec3c8e8be638421d8426afd8a6e8e
Taito Suzuki <suzuito3@gmail.com>
2023-11-29 22:04:37 +0900

    Add crawler_factory_test.go

---
+++ crawler/internal/infra/internal/factory/crawler_factory_test.go
@@ -0,0 +1,111 @@
+package factory
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/suzuito/sandbox2-go/common/test_helper"
+	"github.com/suzuito/sandbox2-go/crawler/pkg/entity/crawler"
+)
+
+func Test(t *testing.T) {
+	testCases := []struct {
+		desc                  string
+		inputFetcherFactory   FetcherFactory
+		inputParserFactory    ParserFactory
+		inputPublisherFactory PublisherFactory
+		inputDef              crawler.CrawlerDefinition
+		expectedError         string
+	}{
+		{
+			desc: "Success",
+			inputFetcherFactory: FetcherFactory{
+				NewFuncs: []NewFuncFetcher{
+					func(_ *crawler.FetcherDefinition, _ *NewFuncFetcherArgument) (crawler.Fetcher, error) {
+						return &DummyFetcher{}, nil
+					},
+				},
+			},
+			inputParserFactory: ParserFactory{
+				NewFuncs: []NewFuncParser{
+					func(_ *crawler.ParserDefinition, _ *NewFuncParserArgument) (crawler.Parser, error) {
+						return &DummyParser{}, nil
+					},
+				},
+			},
+			inputPublisherFactory: PublisherFactory{
+				NewFuncs: []NewFuncPublisher{
+					func(_ *crawler.PublisherDefinition, _ *NewFuncPublisherArgument) (crawler.Publisher, error) {
+						return &DummyPublisher{}, nil
+					},
+				},
+			},
+		},
+		{
+			desc: "Failed to FetcherFactory.Get",
+			inputFetcherFactory: FetcherFactory{
+				NewFuncs: []NewFuncFetcher{
+					func(_ *crawler.FetcherDefinition, _ *NewFuncFetcherArgument) (crawler.Fetcher, error) {
+						return nil, errors.New("dummy")
+					},
+				},
+			},
+			expectedError: "dummy",
+		},
+		{
+			desc: "Failed to ParserFactory.Get",
+			inputFetcherFactory: FetcherFactory{
+				NewFuncs: []NewFuncFetcher{
+					func(_ *crawler.FetcherDefinition, _ *NewFuncFetcherArgument) (crawler.Fetcher, error) {
+						return &DummyFetcher{}, nil
+					},
+				},
+			},
+			inputParserFactory: ParserFactory{
+				NewFuncs: []NewFuncParser{
+					func(_ *crawler.ParserDefinition, _ *NewFuncParserArgument) (crawler.Parser, error) {
+						return nil, errors.New("dummy")
+					},
+				},
+			},
+			expectedError: "dummy",
+		},
+		{
+			desc: "Success",
+			inputFetcherFactory: FetcherFactory{
+				NewFuncs: []NewFuncFetcher{
+					func(_ *crawler.FetcherDefinition, _ *NewFuncFetcherArgument) (crawler.Fetcher, error) {
+						return &DummyFetcher{}, nil
+					},
+				},
+			},
+			inputParserFactory: ParserFactory{
+				NewFuncs: []NewFuncParser{
+					func(_ *crawler.ParserDefinition, _ *NewFuncParserArgument) (crawler.Parser, error) {
+						return &DummyParser{}, nil
+					},
+				},
+			},
+			inputPublisherFactory: PublisherFactory{
+				NewFuncs: []NewFuncPublisher{
+					func(_ *crawler.PublisherDefinition, _ *NewFuncPublisherArgument) (crawler.Publisher, error) {
+						return nil, errors.New("dummy")
+					},
+				},
+			},
+			expectedError: "dummy",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			f := CrawlerFactory{
+				FetcherFactory:   tC.inputFetcherFactory,
+				ParserFactory:    tC.inputParserFactory,
+				PublisherFactory: tC.inputPublisherFactory,
+			}
+			_, err := f.Get(context.Background(), &tC.inputDef)
+			test_helper.AssertError(t, tC.expectedError, err)
+		})
+	}
+}
